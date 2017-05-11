1a2662be30b807b864e14dcca015460887698384
evan <1003026882@qq.com>
2017-05-11 17:38:03 +0800

    Initial Commit

---
+++ EyeHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EyeHelper
+//
+//  Created by evan on 5/11/17.
+//  Copyright © 2017 evan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
